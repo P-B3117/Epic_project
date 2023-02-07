806b5be135d80d74ee6f835344634768968d1e00
Charles <zacse38@gmail.com>
2023-02-07 12:38:51 -0500

    UI_update

---
+++ Assets/UI_Scripts/GameConstants.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Filename : GameConstants
+ * 
+ * Goal : Provides a public single instance of settings that stays the same between scenes
+ * 
+ */
+public static class GameConstants
+{
+    public static float MusicVolume = 0;
+    public static float SoundEffectVolume = 0;
+    public static bool Fullscreen = true;
+
+}
--- Assets/UI_Scripts/UiManager.cs
+++ Assets/UI_Scripts/UiManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
     public GameObject MenuPanel;
     public GameObject SettingsPanel;
+    public GameObject FullscreenToggle;
     // Start is called before the first frame update
     void Start()
     {
         MenuPanel.SetActive(true);
         SettingsPanel.SetActive(false);
+        Screen.SetResolution(1920, 1080, GameConstants.Fullscreen, 60); //int width, int height, bool fullscreen, int preferredRefreshRate (0 = unlimited)
+        FullscreenToggle.GetComponent<Toggle>().isOn = true;
     }
 
     public void ShowSettingsPanel()
@@ -23,5 +27,18 @@ public class UiManager : MonoBehaviour
     {
         MenuPanel.SetActive(true);
         SettingsPanel.SetActive(false);
+    }
+
+    public void ChangeFullscreenState(bool check)
+    {
+        if (GameConstants.Fullscreen == false) { 
+            GameConstants.Fullscreen = true;
+            Debug.Log("Fullscreen state changed to true");
+        }
+        else { 
+            GameConstants.Fullscreen = false;
+            Debug.Log("Fullscreen state changed to false");
+        }
+        Screen.SetResolution(1920, 1080, GameConstants.Fullscreen, 60);
     }
 }
