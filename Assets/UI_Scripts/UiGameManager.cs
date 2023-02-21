26607576ca63204cadd7ec1d1845484a55cb0c66
Charles <zacse38@gmail.com>
2023-02-21 08:53:39 -0500

    iur3h

---
+++ Assets/UI_Scripts/UiGameManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class UiGameManager : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject SettingsPanel;
+    public GameObject FullscreenToggle;
+    public GameObject MusicSlider;
+    public GameObject SoundEffectSlider;
+    public AudioSource MusicSource;
+    public Text MusicVolumeText;
+    public Text SoundEffectVolumeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        Screen.SetResolution(1920, 1080, GameConstants.Fullscreen, 60); //int width, int height, bool fullscreen, int preferredRefreshRate (0 = unlimited)
+        FullscreenToggle.GetComponent<Toggle>().isOn = true;
+        MusicSlider.GetComponent<Slider>().value = GameConstants.MusicVolume;
+        SoundEffectSlider.GetComponent<Slider>().value = GameConstants.SoundEffectVolume;
+    }
+
+    public void ShowSettingsPanel()
+    {
+        MenuPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void ShowMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+
+    public void ChangeFullscreenState(bool check)
+    {
+        if (GameConstants.Fullscreen == false)
+        {
+            GameConstants.Fullscreen = true;
+        }
+        else
+        {
+            GameConstants.Fullscreen = false;
+        }
+        Screen.SetResolution(1920, 1080, GameConstants.Fullscreen, 60);
+    }
+
+    public void MusicVolumeUpdate(float value)
+    {
+        MusicVolumeText.text = Mathf.RoundToInt(value * 100) + "%";
+        GameConstants.MusicVolume = value * 100;
+        MusicSource.volume = Mathf.Log10(float.Parse(value.ToString()));
+    }
+
+    public void SoundEffectVolumeUpdate(float value)
+    {
+        SoundEffectVolumeText.text = Mathf.RoundToInt(value * 100) + "%";
+        GameConstants.SoundEffectVolume = value * 100;
+    }
+}
