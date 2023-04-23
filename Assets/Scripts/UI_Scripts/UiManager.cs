3b44c666da077e3aa84869dbb1fb394ee215e9d8
Charles <zacse38@gmail.com>
2023-04-23 15:43:43 -0400

    jsajdlka

--- Assets/Scripts/UI_Scripts/UiManager.cs
+++ Assets/Scripts/UI_Scripts/UiManager.cs
@@ -15,14 +15,17 @@ public class UiManager : MonoBehaviour
     public AudioSource MusicSource;
     public TextMeshProUGUI MusicVolumeText;
     public TextMeshProUGUI SoundEffectVolumeText;
+    public TextMeshProUGUI Title;
 
     private Vector3 slidedPosition;
     private Vector3 retractedPosition;
+    private Vector3 titleSlidedPosition;
+    private Vector3 titleRetractedPosition;
     private bool menuSlided;
     private bool menuRetracted;
     private bool menuHasToSlide = false;
     private bool menuHasToRetract = false;
-    private float slideSpeed = 10;
+    private float slideSpeed = 6;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,15 +38,17 @@ public class UiManager : MonoBehaviour
         menuSlided = false;
         slidedPosition = new Vector3(256, 540, 0);
         retractedPosition = new Vector3(-164, 540, 0);
+        titleSlidedPosition = new Vector3(1200, 900, 0);
+        titleRetractedPosition = new Vector3(980, 900, 0);
     }
     public void Update()
     {
         Debug.Log(MenuPanel.transform.position.x);
-        if (!menuSlided && menuHasToSlide) MenuPanel.transform.position = Vector3.MoveTowards(MenuPanel.transform.position, slidedPosition, slideSpeed);
-        if (MenuPanel.transform.position == slidedPosition) { menuSlided = true; menuHasToSlide = false; }
+        if (!menuSlided && menuHasToSlide) { MenuPanel.transform.position = Vector3.MoveTowards(MenuPanel.transform.position, slidedPosition, slideSpeed); Title.transform.position = Vector3.MoveTowards(Title.transform.position, titleSlidedPosition, slideSpeed); }
+            if (MenuPanel.transform.position == slidedPosition) { menuSlided = true; menuHasToSlide = false; }
         else menuSlided = false;
 
-        if (!menuRetracted && menuHasToRetract) MenuPanel.transform.position = Vector3.MoveTowards(MenuPanel.transform.position, retractedPosition, slideSpeed);
+        if (!menuRetracted && menuHasToRetract) { MenuPanel.transform.position = Vector3.MoveTowards(MenuPanel.transform.position, retractedPosition, slideSpeed); Title.transform.position = Vector3.MoveTowards(Title.transform.position, titleRetractedPosition, slideSpeed); }
         if (MenuPanel.transform.position == retractedPosition) { menuRetracted = true; menuHasToRetract = false; }
         else menuRetracted = false;
     }
